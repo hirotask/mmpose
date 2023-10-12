aae84c8aaba4838da416c4dc09cc976a51d05c81
hirotask <hirotaskhashi@gmail.com>
2023-10-12 22:29:14 +0900

    datasetの設定を変更

--- mmpose/datasets/datasets/custom/custom_dataset.py
+++ mmpose/datasets/datasets/custom/custom_dataset.py
@@ -1,6 +1,6 @@
-from mmengine.dataset import BaseDataset
+from ..base import BaseCocoStyleDataset
 from mmpose.registry import DATASETS
 
 @DATASETS.register_module(name='CustomDataset')
-class CustomDataset(BaseDataset):
+class CustomDataset(BaseCocoStyleDataset):
     METAINFO: dict = dict(from_file='configs/_base_/datasets/custom_dataset.py')
