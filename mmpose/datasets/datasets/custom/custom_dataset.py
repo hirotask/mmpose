7111aaa7df6dda13d8fc20f2cfbab614eb6f54cf
hirotask <hirotaskhashi@gmail.com>
2023-11-26 00:23:15 +0900

    customdatasetの親クラスを変更

--- mmpose/datasets/datasets/custom/custom_dataset.py
+++ mmpose/datasets/datasets/custom/custom_dataset.py
@@ -1,6 +1,6 @@
-from ..base import BaseCocoStyleDataset
+from mmengine.dataset import BaseDataset
 from mmpose.registry import DATASETS
 
 @DATASETS.register_module(name='CustomDataset')
-class CustomDataset(BaseCocoStyleDataset):
+class CustomDataset(BaseDataset):
     METAINFO: dict = dict(from_file='configs/_base_/datasets/custom_dataset.py')
