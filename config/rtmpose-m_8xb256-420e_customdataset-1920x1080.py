cc93d7ee14f5c4df2cbb252110389e4e4504229e
hirotask <hirotaskhashi@gmail.com>
2023-12-09 13:36:04 +0900

    pipelineの修正

--- config/rtmpose-m_8xb256-420e_customdataset-1920x1080.py
+++ config/rtmpose-m_8xb256-420e_customdataset-1920x1080.py
@@ -122,6 +122,21 @@ train_pipeline = [
     dict(
         type='RandomBBoxTransform', scale_factor=[0.6, 1.4], rotate_factor=80),
     dict(type='TopdownAffine', input_size=codec['input_size']),
+        dict(
+        type='Albumentation',
+        transforms=[
+            dict(type='Blur', p=0.1),
+            dict(type='MedianBlur', p=0.1),
+            dict(
+                type='CoarseDropout',
+                max_holes=1,
+                max_height=0.4,
+                max_width=0.4,
+                min_holes=1,
+                min_height=0.2,
+                min_width=0.2,
+                p=1.),
+        ]),
     dict(type='mmdet.YOLOXHSVRandomAug'),
     dict(type='GenerateTarget', encoder=codec),
     dict(type='PackPoseInputs')
@@ -129,27 +144,7 @@ train_pipeline = [
 val_pipeline = [
     dict(type='LoadImage', backend_args=backend_args),
     dict(type='GetBBoxCenterScale'),
-    dict(type='RandomFlip', direction='horizontal'),
-    dict(type='RandomHalfBody'),
-    dict(
-        type='RandomBBoxTransform', scale_factor=[0.6, 1.4], rotate_factor=80),
     dict(type='TopdownAffine', input_size=codec['input_size']),
-    dict(type='mmdet.YOLOXHSVRandomAug'),
-    # dict(
-    #     type='Albumentation',
-    #     transforms=[
-    #         dict(type='Blur', p=0.1),
-    #         dict(type='MedianBlur', p=0.1),
-    #         dict(
-    #             type='CoarseDropout',
-    #             max_holes=1,
-    #             max_height=0.4,
-    #             max_width=0.4,
-    #             min_holes=1,
-    #             min_height=0.2,
-    #             min_width=0.2,
-    #             p=1.),
-    #     ]),
     dict(type='PackPoseInputs')
 ]
 
@@ -165,21 +160,21 @@ train_pipeline_stage2 = [
         rotate_factor=60),
     dict(type='TopdownAffine', input_size=codec['input_size']),
     dict(type='mmdet.YOLOXHSVRandomAug'),
-    # dict(
-    #     type='Albumentation',
-    #     transforms=[
-    #         dict(type='Blur', p=0.1),
-    #         dict(type='MedianBlur', p=0.1),
-    #         dict(
-    #             type='CoarseDropout',
-    #             max_holes=1,
-    #             max_height=0.4,
-    #             max_width=0.4,
-    #             min_holes=1,
-    #             min_height=0.2,
-    #             min_width=0.2,
-    #             p=0.5),
-    #     ]),
+    dict(
+        type='Albumentation',
+        transforms=[
+            dict(type='Blur', p=0.1),
+            dict(type='MedianBlur', p=0.1),
+            dict(
+                type='CoarseDropout',
+                max_holes=1,
+                max_height=0.4,
+                max_width=0.4,
+                min_holes=1,
+                min_height=0.2,
+                min_width=0.2,
+                p=0.5),
+        ]),
     dict(type='GenerateTarget', encoder=codec),
     dict(type='PackPoseInputs')
 ]
